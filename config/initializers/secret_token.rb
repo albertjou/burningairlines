ec22163c37b7fc2837f18192cd044a3d580746fb
Nick Doherty <nickdoherty@gmail.com>
2013-11-28 16:20:50 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirlinesApp::Application.config.secret_token = '9cb6c9020c9dc8697bfa6cc7a817bc379ced2b37fe04053672af1ab7cb4c623b2117855c0c19046969186723c639892df89d29faff576ca166c9e65f3066a210'
---
+++ db/migrate/20131128043922_add_planes_table.rb
@@ -0,0 +1,8 @@
+class AddPlanesTable < ActiveRecord::Migration
+  def change
+      create_table :planes do |t|
+      t.string :model_number
+      t.string :status
+      t.timestamps
+  end
+end
