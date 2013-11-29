6d2d0b9f682033ab15cc02bc1f83715a87e1ff96
albertjou <albert_jou@hotmail.com>
2013-11-29 16:14:48 +1100

    Included the FlightSelectionView in Backbone

---
+++ app/assets/javascripts/Backbone/models/Flights.js
@@ -0,0 +1,20 @@
+var flightApp = flightApp || {
+
+};
+
+flightApp.Reservation = Backbone.Model.extend({
+  urlRoot: '/reservations',
+  defaults: {
+    flight_id: "",
+    user_id: "",
+    meal: "",
+    seat_number: "",
+    notes: ""
+  }
+
+});
+
+flightApp.Reservations = Backbone.Collection.extend({
+  model: flightApp.Reservation,
+  url: '/reservations'
+})
---
+++ app/assets/javascripts/Backbone/views/flightSelectionView.js
@@ -0,0 +1,17 @@
+var flightApp = flightApp || {
+
+};
+
+// This is the view that will generate the seats
+
+flightApp.flightSelectionView = Backbone.View.extend({
+  el: '#flight-selection',
+  events: {},
+  initialize: function () {
+
+  },
+  render: function () {
+
+  }
+});
+
