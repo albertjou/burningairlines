381889fae88709b4efe6619163aaac1888865e0a
albertjou <albert_jou@hotmail.com>
2013-11-29 12:24:51 +1100

    Updated routes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,4 +4,14 @@ class FlightsController < ApplicationController
   def index
   end
 
+  def show
+    @flight = Flight.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @flight, include: [:reservations] }
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 AirlinesApp::Application.routes.draw do
+  root :to => 'pages#home'
+
+  resources :flights, :reservations
   devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
   ActiveAdmin.routes(self)
 
 
-  root :to => 'pages#home'
 
 
 end
