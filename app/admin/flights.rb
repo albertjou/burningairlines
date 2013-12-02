8ff5bfabab58db613b402798c348f161875f18cf
Nick Doherty <nickdoherty@gmail.com>
2013-12-02 15:09:04 +1100

    final admin section

--- app/admin/flights.rb
+++ app/admin/flights.rb
@@ -1,17 +1,20 @@
 ActiveAdmin.register Flight do
   index do
-    column :plane_id
     column "Leaving at", :date_time
     column :origin
     column :destination
     default_actions
   end
 
-  show do
-    h3 flight.origin
-    h3 flight.destination
-    h3 flight.date_time
 
-    render "passenger_list"
+
+  show do |ad|
+    attributes_table do
+      row :origin
+      row :destination
+    end
+      render "passenger_list"
   end
+
+  filter :plane, :collection => proc {(Plane.all).map{|p| [p.model_number, p.id]}}
 end
